c08b73b48854ea57c1918e00b9a7987c255bbc1f
behailu-2023 <142741099+behailu-2023@users.noreply.github.com>
2024-07-21 22:12:33 +0200

    Update on Chat User Interface

--- components/Chat.js
+++ components/Chat.js
@@ -1,31 +1,78 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
 
 const Chat = ({ route, navigation }) => {
+  const [messages, setMessages] = useState([]);
   const { name, backgroundColor } = route.params;
 
   useEffect(() => {
-    navigation.setOptions({ title: name });
-  }, [name]);
+    navigation.setOptions({ title: name })
+    setMessages([
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: 'This is a system message',
+          createdAt: new Date(),
+          system: true,
+        },
+      ]);
+  }, [name, navigation]);
+
+  const onSend = (newMessages = []) => {
+    setMessages((previousMessages) => GiftedChat.append(previousMessages, newMessages));
+  };
+  const renderBubble = (props) => {
+    return (
+    <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000"
+        },
+        left: {
+          backgroundColor: "#FFF"
+        },
+      }}
+    />
+    );
+  }
 
   return (
     <View style={[styles.container, { backgroundColor }]}>
+        <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+          name
+        }}
+      />
+      {Platform.OS === "ios"?<KeyboardAvoidingView behavior="padding" />: null}
       <Text style={styles.text}>Welcome, {name}!</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-});
+    container: {
+      flex: 1
+    },
+    text: {
+        textAlign: 'center',
+        marginBottom: 10,
+        color: '#000',
+      },
+  });
 
 export default Chat;
